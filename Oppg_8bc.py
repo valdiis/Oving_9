3d593b2f1beb2ac7522cc8e38f9c004f5ed2a340
Mathias Valderhaug <mathiasvalderhaug@Mathiass-MacBook-Pro.local>
2021-11-05 13:51:32 +0100

    hei

--- Oppg_8bc.py
+++ Oppg_8bc.py
@@ -6,21 +6,48 @@ Created on Thu Oct 21 16:19:23 2021
 @author: mathiasvalderhaug
 """
 
-class Que():
-    def __init__(self, que, ans, alt):
+from typing import Counter
+
+
+text_fila = open("spørsmaalsfil.txt", "r", encoding="UTF8" ")
+question = []
+nevner = []
+svar1 = []
+
+for linje in text_fila.readlines():
+    tekst = linje.split(":")
+    question.append(tekst[0])
+    nevner.append(tekst[1])
+
+    svar1_rad = teks[2].split(",")
+    svar1.append(svar1_rad)
+
+nevner = [i.strip(" ")] for i in nevner]
+
+class Quiz():
+    # __init__() blir kalt automatisk kvar gang classen blir brukt for å lage et nytt objekt 
+    def __init__(self, que, opt, ans):
         self.question = que
+        self.option = opt
         self.answer = ans
-        self.alternative = alt
         
     def __str__(self):
+        #lager en god representasjon av spørsmål og alternativ
+        
+        #spørsmål på første linjen 
         tekst = self.question + "\n"
-        for alternativnummer, alternativsvar, in enumerate(self.alternative):
-            tekst += f"{alternativnummer+1}: {alternativsvar} \n"
+        
+        for counter in range(len(self.options)):
+            tekst += f"{counter}- {self.options[counter]} \n"
         return tekst
-    def sjekk_svar (self, sjekk: int):
-        return sjekk == self.answer
+
+    def sjekk_svar (self, sjekk):
+        if sjekk == self.answer
+        print("Korrekt svar: " + self.options[int(self.answer)])
+
+
     
-quiz = []
+spill = []
 
 quiz.append(Que("Hva er hovedstaden i Norge?", 2, ["Bergen", "Oslo", "Trondheim", "Ålesund"]))
 quiz.append(Que("Hvor ligger England", 3, ["Ålesund", "Asia", "Europa", "Afrika"]))
